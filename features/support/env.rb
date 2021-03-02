7a2f14adb32e9c2dbc671f0bf97cb27057f3012a
danilov.and <danilov032.andrey@gmail.com>
2021-03-02 16:40:45 +0300

    Доработаны наброски, реализованы скриншоты

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,3 +14,31 @@ end
 
 Appium::Driver.new(caps, true)
 Appium.promote_appium_methods Object
+
+def exist_element?(element)
+  begin
+    return true if find_element(element)
+  rescue
+    return false
+  end
+end
+
+def waiting_element_id_with_stop(id, time)
+  spent_time = 0
+  until exist_element?(id: id)
+    sleep (0.5)
+    spent_time += 1
+    if spent_time.to_i > time.to_i
+      raise "Элемент #{id} не появился спустя #{spent_time} секунд"
+    end
+  end
+end
+
+def waiting (id, time)
+  spent_time = 0
+  until exist_element?(id: id)
+    sleep(0.5)
+    spent_time += 1
+    break if spent_time.to_i > time.to_i
+  end
+end
